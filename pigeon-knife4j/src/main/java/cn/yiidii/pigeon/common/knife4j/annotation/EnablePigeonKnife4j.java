3a52f727af69feeed8b42937a036a032c132215d
wangyidi0731 <wangyiidii@163.com>
2021-02-15 16:04:05 +0800

    knife接口文档starter模块

---
+++ pigeon-knife4j/src/main/java/cn/yiidii/pigeon/common/knife4j/annotation/EnablePigeonKnife4j.java
@@ -0,0 +1,23 @@
+package cn.yiidii.pigeon.common.knife4j.annotation;
+
+import cn.yiidii.pigeon.common.knife4j.config.Knife4jConfig;
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.lang.annotation.*;
+
+/**
+ * @author: YiiDii Wang
+ * @create: 2021-02-15 11:47
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@EnableSwagger2
+@EnableKnife4j
+@Import({Knife4jConfig.class})
+public @interface EnablePigeonKnife4j {
+}
