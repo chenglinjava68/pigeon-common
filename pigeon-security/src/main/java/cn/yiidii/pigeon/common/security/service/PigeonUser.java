807d0b59733d801513c043659d8195eba2817855
wangyidi0731 <wangyiidii@163.com>
2021-02-16 01:03:26 +0800

    提供UserDTO转换PigeonUser的方法

--- pigeon-security/src/main/java/cn/yiidii/pigeon/common/security/service/PigeonUser.java
+++ pigeon-security/src/main/java/cn/yiidii/pigeon/common/security/service/PigeonUser.java
@@ -1,12 +1,17 @@
 
 package cn.yiidii.pigeon.common.security.service;
 
+import cn.yiidii.pigeon.rbac.api.dto.ResourceDTO;
+import cn.yiidii.pigeon.rbac.api.dto.UserDTO;
 import lombok.Getter;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author lengleng
@@ -14,17 +19,29 @@ import java.util.Collection;
  */
 public class PigeonUser extends User {
 
-	/**
-	 * 用户ID
-	 */
-	@Getter
-	private Long id;
+    /**
+     * 用户ID
+     */
+    @Getter
+    private Long id;
 
-	public PigeonUser(Long id, String username, String password, boolean enabled,
-					  boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked,
-					  Collection<? extends GrantedAuthority> authorities) {
-		super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
-		this.id = id;
-	}
+    public PigeonUser(Long id, String username, String password, boolean enabled,
+                      boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked,
+                      Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+        this.id = id;
+    }
+
+    public static PigeonUser transPigeonUser(UserDTO userDTO) {
+        //根据用户的id查询用户的权限
+        List<ResourceDTO> resourceDTOs = userDTO.getResources();
+        List<String> resources = resourceDTOs.stream().map(ResourceDTO::getCode).collect(Collectors.toList());
+        //将permissions转成数组
+        String[] permissionArray = new String[resources.size()];
+        resources.toArray(permissionArray);
+
+        return new PigeonUser(userDTO.getId(), userDTO.getUsername(), userDTO.getPassword(),
+                true, true, true, true, AuthorityUtils.createAuthorityList(permissionArray));
+    }
 
 }
