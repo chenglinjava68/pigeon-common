7205c3afadd840de4072bc350eeac2afebd0ed90
wangyidi0731 <wangyiidii@163.com>
2021-02-16 20:13:28 +0800

    未登录

--- pigeon-security/src/main/java/cn/yiidii/pigeon/common/security/handler/PigeonAuthenticationEntryPoint.java
+++ pigeon-security/src/main/java/cn/yiidii/pigeon/common/security/handler/PigeonAuthenticationEntryPoint.java
@@ -1,6 +1,7 @@
 package cn.yiidii.pigeon.common.security.handler;
 
 import cn.yiidii.pigeon.common.core.base.R;
+import cn.yiidii.pigeon.common.core.exception.code.ExceptionCode;
 import cn.yiidii.pigeon.common.core.util.WebUtils;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -20,7 +21,7 @@ public class PigeonAuthenticationEntryPoint implements AuthenticationEntryPoint 
 
     @Override
     public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
-        R result = R.failed(300, e.getMessage());
+        R result = R.failed(ExceptionCode.JWT_NOT_LOGIN.getCode(), ExceptionCode.JWT_NOT_LOGIN.getMsg());
         WebUtils.renderJson(httpServletResponse, result);
     }
 
